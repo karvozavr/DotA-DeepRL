d3501efd4d8b4985d5d089e9ab063412e216eaf3
Dmitriy Abramov <karvozavr@gmail.com>
2018-04-12 00:20:41 +0300

    Cm

--- dota2-env/dota2env.py
+++ dota2-env/dota2env.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from dota2.protobufs.dota_shared_enums_pb2 import DOTA_GC_TEAM, DOTABotDifficulty
+# from dota2.protobufs.dota_shared_enums_pb2 import DOTA_GC_TEAM, DOTABotDifficulty
 from steam import SteamClient
 from dota2 import Dota2Client
+from dota2.enums import DOTA_GC_TEAM, DOTABotDifficulty
 
 # Setup logging.
 import logging
@@ -10,7 +11,7 @@ import logging
 logging.basicConfig(format='[%(asctime)s] %(levelname)s %(name)s: %(message)s', level=logging.DEBUG)
 
 client = SteamClient()
-dota = Dota2Client()
+dota = Dota2Client(client)
 
 
 @client.on('logged_on')
@@ -21,18 +22,18 @@ def start_dota():
 @dota.on('ready')
 def dota_launched():
     # Create game lobby.
-    dota.create_practice_lobby(options={'id'})
+    dota.create_practice_lobby()
 
 
 @dota.on('lobby_new')
-def entered_lobby():
+def entered_lobby(lobby):
     """
     Entered a lobby event handler (by creating or joining one).
     """
 
     # Add bots and yourself to lobby.
     dota.join_practice_lobby_team(slot=2,
-                                  team=DOTA_GC_TEAM.SPECTATOR)
+                                  team=DOTA_GC_TEAM.SPECTATORS)
     dota.add_bot_to_practice_lobby(slot=1,
                                    team=DOTA_GC_TEAM.GOOD_GUYS,
                                    bot_difficulty=DOTABotDifficulty.BOT_DIFFICULTY_PASSIVE)
@@ -44,9 +45,9 @@ def entered_lobby():
 
 
 def main():
-    # TODO
-    pass
-
+    # FIXME DO NOT PUBLISH
+    client.cli_login(username='vergiliy57', password='575Vfcnth')
+    client.run_forever()
 
 if __name__ == '__main__':
     main()
---
+++ dota2-env/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'POST':
+        data = {'lol': 22, 'kek': 42, 'cheburek': 239}
+        return jsonify(data)
+    return 'Error'
